377ad2f3e66d4ea0e9e959690244ab7177ddf20e
a1aw <12967084+a1aw@users.noreply.github.com>
2024-11-26 06:40:15 +0800

    Add more script checks

--- start_build_tool.sh
+++ start_build_tool.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 git submodule update --init --recursive
 cd vesc_fw
 if [ ! -d ./tools/gcc-arm-none-eabi-7-2018-q2-update ]; then
@@ -6,8 +7,8 @@ if [ ! -d ./tools/gcc-arm-none-eabi-7-2018-q2-update ]; then
 fi
 cd ..
 cd vesc_tool
-git apply ../patches/vesc_tool/0001-build-cp-fw.patch
-git apply ../patches/vesc_tool/0002-lzokay-gcc12-support.patch
+git apply --reverse --check ../patches/vesc_tool/0001-build-cp-fw.patch || git apply ../patches/vesc_tool/0001-build-cp-fw.patch
+git apply --reverse --check ../patches/vesc_tool/0002-lzokay-gcc12-support.patch || git apply ../patches/vesc_tool/0001-build-cp-fw.patch
 cd ..
 docker build . -t vesc_builder --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER})
 docker run --rm -ti -v $(pwd):/ws vesc_builder /ws/docker_entrypoint.sh
