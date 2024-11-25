6b083c9f6690a1d3b4247d93924685a52a7786cc
a1aw <12967084+a1aw@users.noreply.github.com>
2024-11-26 04:05:08 +0800

    Run arm sdk install if not exist

--- start_build_tool.sh
+++ start_build_tool.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 git submodule update --init --recursive
+cd vesc_fw
+if [ ! -d ./tools/gcc-arm-none-eabi-7-2018-q2-update ]; then
+    make arm_sdk_install
+fi
+cd ..
 cd vesc_tool
 git apply ../patches/vesc_tool/0001-build-cp-fw.patch
 git apply ../patches/vesc_tool/0002-lzokay-gcc12-support.patch
