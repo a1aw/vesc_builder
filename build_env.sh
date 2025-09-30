a42f547e43f3f084915f75e5c752c0a94a5fce49
a1aw <12967084+a1aw@users.noreply.github.com>
2025-09-30 09:03:44 +0800

    x

--- build_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-docker build . -t vesc_builder --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER})
--- scripts/start_build_linux.sh
+++ scripts/start_build_linux.sh
@@ -10,4 +10,4 @@ cd vesc_tool
 git apply --reverse --check ../patches/vesc_tool/0001-build-cp-fw.patch || git apply ../patches/vesc_tool/0001-build-cp-fw.patch
 git apply --reverse --check ../patches/vesc_tool/0002-lzokay-gcc12-support.patch || git apply ../patches/vesc_tool/0001-build-cp-fw.patch
 cd ..
-docker run --rm -ti -v $(pwd):/ws vesc_builder /ws/docker_entrypoint.sh
+docker run --rm -ti -v $(pwd):/ws vesc-builder-ubuntu-noble /ws/scripts/linux_build_entrypoint.sh
